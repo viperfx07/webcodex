832a124a9c34a0724b637b5ca908058a112e5fdc
Victor <victor@webcoda.com.au>
2019-06-05 17:54:41 +1000

    update icon style

--- content_script.js
+++ content_script.js
@@ -6,15 +6,15 @@
     if (window.location.href.includes('bugherd')) {
         const timerLi = document.createElement('li')
         timerLi.className = 'miniButton'
-        timerLi.innerHTML = `<button id="timerbtn" type="button" tabIndex="-1">T</button>`
+        timerLi.innerHTML = `<button id="timerbtn" type="button" tabIndex="-1" style="display: inline-flex; align-items: center; justify-content: center; background: none;"><svg width="12" height="12" focusable="false" viewBox="0 0 32 32"><path fill="#6f8694" d="M26.6,8.8l2.3-2.3c0.4-0.4,0.4-1,0-1.4l0,0c-0.4-0.4-1-0.4-1.4,0l-2.3,2.3C22.7,5.3,19.5,4,16,4S9.3,5.3,6.8,7.4L4.6,5.2c-0.4-0.4-1-0.4-1.4,0l0,0c-0.4,0.4-0.4,1,0,1.4l2.3,2.3c-2.6,3-3.9,7-3.3,11.4c1,6.1,6,11,12.2,11.7C22.8,32.9,30,26.3,30,18C30,14.5,28.7,11.3,26.6,8.8z M16,30C9.4,30,4,24.6,4,18S9.4,6,16,6s12,5.4,12,12S22.6,30,16,30z M13,0h6c0.6,0,1,0.4,1,1l0,0c0,0.6-0.4,1-1,1h-6c-0.6,0-1-0.4-1-1l0,0C12,0.4,12.4,0,13,0z M16,17l5.2-5.2c0.4-0.4,1-0.4,1.4,0l0,0c0.4,0.4,0.4,1,0,1.4l-5.2,5.2c-0.4,0.4-1,0.4-1.4,0l0,0C15.6,18,15.6,17.3,16,17z M16,8c0.6,0,1,0.4,1,1s-0.4,1-1,1c-0.6,0-1-0.4-1-1S15.4,8,16,8z M16,26c0.6,0,1,0.4,1,1s-0.4,1-1,1c-0.6,0-1-0.4-1-1S15.4,26,16,26z M9.6,10.6c0.6,0,1,0.4,1,1s-0.4,1-1,1s-1-0.4-1-1S9,10.6,9.6,10.6z M22.4,23.4c0.6,0,1,0.4,1,1s-0.4,1-1,1s-1-0.4-1-1S21.8,23.4,22.4,23.4z M7,17c0.6,0,1,0.4,1,1s-0.4,1-1,1s-1-0.4-1-1S6.4,17,7,17z M25,17c0.6,0,1,0.4,1,1s-0.4,1-1,1s-1-0.4-1-1S24.4,17,25,17z M9.6,23.4c0.6,0,1,0.4,1,1s-0.4,1-1,1s-1-0.4-1-1S9,23.4,9.6,23.4z"></path></svg></button>`
 
         const timerDialog = document.createElement('dialog')
         timerDialog.style.padding = 0
         timerDialog.style.border = 0
         timerDialog.style.minWidth = '500px'
 
-        const parent = document.querySelector('.panelHeadActions')
-        parent.insertBefore(timerLi, parent.firstChild)
+        const parent = document.querySelector('.panelHead .panelHeadActions')
+        parent.insertBefore(timerLi, parent.lastChild)
         document.body.append(timerDialog)
 
         window.addEventListener('message', ev => {
